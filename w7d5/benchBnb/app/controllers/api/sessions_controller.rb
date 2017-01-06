fe02184b02d99fd9a0b1fcd59726b22c685eb1c9
Nate Reiners <nate.reiners@gmail.com>
2017-01-06 14:28:21 -0800

    add missing gemfiles better_errors etc

--- w7d5/benchBnb/app/controllers/api/sessions_controller.rb
+++ w7d5/benchBnb/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,20 @@
 class Api::SessionsController < ApplicationController
+
+
+  def create
+    @session = Session.new(
+    params.session[:username],
+    params.session[:password]
+    )
+    if @session.create!
+    else
+      flash[:errors] = @session.errors.full_messages
+    end
+  end
+
+  def destroy
+    logout current_user;
+  end
+
+
 end
--- w7d5/benchBnb/app/controllers/api/users_controller.rb
+++ w7d5/benchBnb/app/controllers/api/users_controller.rb
@@ -1,2 +1,20 @@
 class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params);
+    if @user.create!
+      login(@user)
+    else
+      flash[:errors] = @user.errors.full_messages
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
