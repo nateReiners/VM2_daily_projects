60918174c813ecc8873aaa111463952e17a001a7
Nate Reiners <nate.reiners@gmail.com>
2017-01-09 18:35:27 -0800

    add session_form and session_form_container

---
+++ w8d1/benchBnb/frontend/components/session_form/session_form.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = { username: "", password: "" };
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.redirectIfLoggedIn();
+  }
+
+  redirectIfLoggedIn() {
+    if (this.props.loggedIn) {
+      this.props.router.push("/");
+    }
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    console.log(e);
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user).then(() => this.redirect());
+  }
+
+  navLink() {
+    if (this.props.formType === "login") {
+      return <Link to="/signup">Sign Up instead</Link>;
+    } else {
+      return <Link to="/login">Log In instead</Link>;
+    }
+  }
+
+  renderErrors() {
+    return (
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            { error }
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+
+  render () {
+    return (
+      <div className="login-form-container">
+        <form onSubmit={this.handleSubmit} className="login-form-box">
+          <br/>
+          Welcome to Nate's BenchBnb!!!
+          <br/>
+          <br/>
+          Please {this.props.formType} or {this.navLink()}
+          {this.renderErrors()}
+          <div className="login-form">
+            <br/>
+            <label> Username:
+              <input type="text"
+                    value={this.state.username}
+                    onChange={this.update("username")}
+                    ClassName="login-input" />
+            </label>
+            <br/>
+            <label> Password:
+              <input type="password"
+                    value={this.state.password}
+                    onChange={this.update("password")}
+                    ClassName="login-input" />
+            </label>
+            <br/>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default SessionForm;
